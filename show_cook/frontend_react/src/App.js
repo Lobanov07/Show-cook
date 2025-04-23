91fea64524c65fef6703044cbd07134ead33c699
4315 Ледовских А.В. <ledartem@bk.ru>
2025-04-23 23:07:23 +0300

    Create react-app

---
+++ show_cook/frontend_react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="show-cook">
+        <div className='overlap-wrapper'>
+          <div className='overlap'>
+            <div className='overlap-group'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
