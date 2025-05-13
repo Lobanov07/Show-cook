b880222dafa4748db53e2dc358e559ead360d2f4
4315 Лобанов К.Е. <lobanov1107@yandex.ru>
2025-05-13 13:31:38 +0300

    Main Endpoint Update

--- show_cook/show_cook/settings.py
+++ show_cook/show_cook/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_bootstrap5',
     "corsheaders",
     "rest_framework",
     'rest_framework.authtoken',
@@ -131,7 +132,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
